3af532c1d51a914b466abf44aa92ce631e8109e3
Chris <crbonev@gmail.com>
2022-11-02 14:31:09 +0200

    added tasks

---
+++ 05_functions_advanced/Lab/03_cheese_showcase.py
@@ -0,0 +1,20 @@
+def sorting_cheeses(**kwargs):
+    sorted_cheeses = sorted(
+        kwargs.items(),
+        key=lambda x: (-len(x[1]), x[0]),
+    )
+    result = []
+    for name, pieces in sorted_cheeses:
+        result.append(name)
+        quantity_list = sorted(pieces, reverse=True)
+        result += quantity_list
+    return '\n'.join([str(x) for x in result])
+
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
